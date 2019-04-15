a9a60a59f6bb55cdba682721b6f9f9ac7a616d7a
rebecca <15850680911@163.com>
2019-04-15 23:17:49 +0800

    refresh and add test2

--- sort/QuikSort.py
+++ sort/QuikSort.py
@@ -12,19 +12,29 @@ def QuikSort(a):
         if right>pivot:
             stack.append(right)
             stack.append(pivot+1)
+def QuikSort2(a,left,right):#递归
+    if left==right:
+        return
+    pivot=Partition(a,left,right)
+    if(left<pivot):
+        QuikSort2(a,left,pivot-1)
+    if(right>pivot):
+        QuikSort2(a,pivot+1,right)
 def Partition(a,left,right):
     temp=a[left]
     while left< right:
-        while a[right]>=temp and left<right:
+        while a[right]>=temp and left<right:#取了等于号，使得我不用走完left+=1
             right-=1
         a[left]=a[right]
-        left+=1
-        while a[left]<=temp and left<right:
+        while a[left]<=temp and left<right:#取了等于号，使得我不用走完right-=1
             left+=1
         a[right]=a[left]
-        right-=1
+    a[left]=temp
     return left
+
 if __name__ == '__main__':
     a=[38,49,65,97,76,13,27,49]
     QuikSort(a)
+    print(a)
+    QuikSort2(a,0,len(a)-1)
     print(a)
